d036c0e95429e82a50cc3bce25e4f2a52f5e956a
monkphin <darren.burrows@gmail.com>
2025-04-09 17:02:42 +0100

    updated index page and added logic to show forthcoming box, added users app

--- home/views.py
+++ home/views.py
@@ -3,11 +3,28 @@ from boxes.models import Box
 from datetime import date
 
 def home(request):
+    today = date.today()
 
-    box = Box.objects.filter(is_archived=False, shipping_date__lte=date.today()).order_by('-shipping_date').first()
+    # Current box if shipped
+    box = Box.objects.filter(is_archived=False, shipping_date__lte=today).order_by('-shipping_date').first()
     box_contents = box.contents.all() if box else []
 
+    # Next month’s box
+    if today.month == 12:
+        next_month = 1
+        next_year = today.year + 1
+    else:
+        next_month = today.month + 1
+        next_year = today.year
+
+    next_box = Box.objects.filter(
+        is_archived=False,
+        shipping_date__year=next_year,
+        shipping_date__month=next_month
+    ).first()
+
     return render(request, 'home/index.html', {
-                  'box': box,
-                  'box_contents': box_contents,
-                  })
+        'box': box,
+        'box_contents': box_contents,
+        'next_box': next_box
+    })
