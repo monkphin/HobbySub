4e221bcb52f10d160d309e4ff977b70abcdaf152
monkphin <darren.burrows@gmail.com>
2025-04-13 16:18:11 +0100

    added and wired up account view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
-# home/urls.py
+# users/urls.py
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
         path('register/', views.register_user, name='register'),
         path('account/', views.account_view, name='account'),
+        path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
