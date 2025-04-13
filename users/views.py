9f59f21bcf04ee408a64d0614f2093b45fbe3fc2
monkphin <darren.burrows@gmail.com>
2025-04-13 15:30:35 +0100

    added basic regiration page and logic

--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import Register
 
 
+def register_user(request):
+    if request.method == 'POST':
+        form = Register(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # Auto login on registration
+            return redirect('home')
+    else:
+        form = Register()
+        
+    return render(request, 'users/register.html', {'form': form})
 
-def register(request):
-    form = UserCreationForm
-    return render(request, 'users/register.html')
