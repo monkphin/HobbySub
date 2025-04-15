545a4e05e77746a1a415d0572e511628c0398ca9
monkphin <darren.burrows@gmail.com>
2025-04-15 09:58:47 +0100

    Added modal for enhanced order details on oorders list

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
       indicators: true
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  });
