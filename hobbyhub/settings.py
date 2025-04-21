edbec85c1631c309b6115e31e921911e2804098d
monkphin <darren.burrows@gmail.com>
2025-04-21 23:23:03 +0100

    Add STATIC_ROOT for Heroku static file handling

--- hobbyhub/settings.py
+++ hobbyhub/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
